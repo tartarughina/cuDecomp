057ea7bd414d636b30b9577c1cc5d1e31a6fe883
Riccardo Strina <riccardo.strina@icloud.com>
2024-12-21 13:06:27 +0100

    Fix rank for printing NCCL timings

--- polaris_install.sh
+++ polaris_install.sh
@@ -4,7 +4,7 @@ cmake .. \
 -DCUDECOMP_BUILD_FORTRAN=OFF \
 -DCUDECOMP_BUILD_EXTRAS=ON \
 -DCUDECOMP_ENABLE_NVSHMEM=OFF \
--DCMAKE_INSTALL_PREFIX=/home/tartarughina/test/cuDecomp
+-DCMAKE_INSTALL_PREFIX=/home/tartarughina/cuDecomp
 
 # UM and Tuning are all implemented as cli parameters there's no need to use preprocessing
 # For the multinode runs of Taylor-Green the process, after the successful initialization of NCCL, hangs during the trnaspose autotuning
